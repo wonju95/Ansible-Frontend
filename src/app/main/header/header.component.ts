608d1d34cef86851944cf431954fd3b1138f3381
wonju95 <whdnjswn25@gmail.com>
2022-11-28 15:56:33 +0900

    bootstrap 템플릿 적용

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  
+  sidebarToggleClick(event: MouseEvent){
+    event.preventDefault();
+    document.body.classList.toggle('sb-sidenav-toggled');
+  }
 
 }
